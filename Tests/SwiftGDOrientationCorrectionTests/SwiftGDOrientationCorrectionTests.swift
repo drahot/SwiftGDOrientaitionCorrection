676c5e48007249ffd05470efbc9613cb36df56bd
hottin <hotterdragon@gmail.com>
2023-06-08 21:16:10 +0900

    update: テストケースと画像の追加

--- Tests/SwiftGDOrientationCorrectionTests/SwiftGDOrientationCorrectionTests.swift
+++ Tests/SwiftGDOrientationCorrectionTests/SwiftGDOrientationCorrectionTests.swift
@@ -8,4 +8,19 @@ import Foundation
 import XCTest
 
 final class SwiftGDOrientationCorrectionTests: XCTest {
+
+    func testExportWithOrientationCorrection() throws {
+        let images = [
+            "IMG_0052.jpg",
+            "IMG_0056.jpg",
+        ]
+        images.forEach {
+            let url = URL(fileURLWithPath: "Tests/SwiftGDOrientationCorrectionTests/Resources/\($0)")
+            let image = Image(url: url)
+            let data = try image?.exportWithOrientationCorrection(url: url, format: .jpeg)
+            XCTAssertNotNil(data)
+        }
+    }
+
+
 }
